3b338de54cefa17565761e75084150a446588804
johnmausr-lang <johnmausr@gmail.com>
2025-12-18 12:19:59 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
+import "./globals.css";
 import type { Metadata, Viewport } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 
 // Подключаем современный шрифт
 const inter = Inter({ subsets: ["latin", "cyrillic"] });
