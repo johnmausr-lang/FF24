43e775b0d7d17362d2cd535ea4f83e9b886fd275
johnmausr-lang <johnmausr@gmail.com>
2025-12-18 15:47:52 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  // важно для Telegram API и iframe карты
-  images: {
-    remotePatterns: [],
-  },
-
-  // App Router стабильно
-  experimental: {
-    appDir: true,
-  },
 }
 
 export default nextConfig
