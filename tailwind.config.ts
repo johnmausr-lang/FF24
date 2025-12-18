6387be944afe7539654e4583dbef49d0f85608ab
johnmausr-lang <johnmausr@gmail.com>
2025-12-18 15:42:53 +0500

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,22 @@
 import type { Config } from "tailwindcss"
 
 const config: Config = {
-  darkMode: ["class"],
   content: [
     "./app/**/*.{ts,tsx}",
-    "./components/**/*.{ts,tsx}", // важно: sections тоже тут
+    "./components/**/*.{ts,tsx}"
   ],
   theme: {
     extend: {
-      borderRadius: {
-        lg: "var(--radius)",
-        md: "calc(var(--radius) - 2px)",
-        sm: "calc(var(--radius) - 4px)",
-      },
       colors: {
-        background: "rgb(var(--background))",
-        foreground: "rgb(var(--foreground))",
         primary: "rgb(var(--primary))",
         accent: "rgb(var(--accent))",
+      },
+      borderRadius: {
+        lg: "var(--radius)",
       },
     },
   },
   plugins: [],
 }
+
 export default config
