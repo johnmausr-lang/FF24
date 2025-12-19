caba264f0816d816e067745862884b54961a1382
johnmausr-lang <johnmausr@gmail.com>
2025-12-19 14:14:26 +0500

    Update ParticlesBackground.tsx

--- components/ParticlesBackground.tsx
+++ components/ParticlesBackground.tsx
@@ -24,12 +24,12 @@ export const ParticlesBackground = () => {
         background: { color: { value: "transparent" } },
         fpsLimit: 120,
         particles: {
-          color: { value: "#E0FF64" },
-          links: { color: "#E0FF64", distance: 150, enable: true, opacity: 0.1, width: 1 },
-          move: { enable: true, speed: 0.5 },
-          number: { density: { enable: true }, value: 30 },
-          opacity: { value: { min: 0.1, max: 0.3 } },
-          size: { value: { min: 1, max: 2 } },
+          color: { value: ["#2563EB", "#E0FF64"] },
+          links: { color: "#2563EB", distance: 150, enable: true, opacity: 0.1, width: 1 },
+          move: { enable: true, speed: 1 },
+          number: { density: { enable: true }, value: 40 },
+          opacity: { value: { min: 0.1, max: 0.4 } },
+          size: { value: { min: 1, max: 3 } },
         },
         detectRetina: true,
       }}
